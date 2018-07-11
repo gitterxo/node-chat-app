26cee66a2dfbfbaa0a8f4276b97848aab667341c
gitterxo <dorinenache@fastmail.com>
2018-07-11 17:14:40 +0300

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+//selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last');
+// heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -18,6 +35,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // console.log('Got new message', message);
     //
@@ -39,6 +57,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var formattedTime = moment(message.createdAt).format('h:mm a');
     //
